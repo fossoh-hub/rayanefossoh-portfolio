a91e61d07b2650c59476e68877aab8638c39f4d1
FOSSOH RAYANE <rayanefossoh07@gmail.com>
2025-01-18 12:51:37 +0100

    mon portfolio

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci de m\'avoir contacté ! Je reviendrai vers vous bientôt.');
+});
